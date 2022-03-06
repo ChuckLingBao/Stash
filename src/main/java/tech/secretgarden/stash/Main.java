7ab185631593efede6bee55a3e83c5d472212a51
ChuckLingBao <95773999+ChuckLingBao@users.noreply.github.com>
2022-03-05 21:52:12 -0800

    update

--- src/main/java/tech/secretgarden/stash/Main.java
+++ src/main/java/tech/secretgarden/stash/Main.java
@@ -1,7 +1,7 @@
 package tech.secretgarden.stash;
 
+import io.github.thebusybiscuit.exoticgarden.ExoticGarden;
 import io.github.thebusybiscuit.slimefun4.implementation.Slimefun;
-import me.mrCookieSlime.ExoticGarden.ExoticGarden;
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
